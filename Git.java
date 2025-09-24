12d893ec6c1ee313db582cbceb3bfaeb42e06cf6
Angela Yuan <ayuan1@hwemail.com>
2025-09-24 13:16:26 -0700

    (GP-2.2): Created a Hash Function
    Implements functionality to initialize a basic Git repository structure, including the 'git' directory, 'objects' directory, 'index' file, and 'HEAD' file.
    Also adds a SHA-1 hashing function to create a hexadecimal representation of a file's path.

--- Git.java
+++ Git.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Git {
 
@@ -13,6 +15,33 @@ public class Git {
         if(!f2.exists()) f2.mkdir();
         if(!f3.exists()) f3.createNewFile();
         if(!f4.exists()) f4.createNewFile();
+    }
+
+    public static String hashFile(String filepath) {
+        try (FileInputStream f = new FileInputStream(filepath)) {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] bt = new byte[8192];
+            int i;
+            while ((i = f.read(bt)) != -1) {
+                md.update(bt, 0, i);
+            }
+            byte[] hashedBytes = md.digest();
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashedBytes) {
+                hexString.append(String.format("%02x", b & 0xff));
+            }
+            return hexString.toString();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found " + filepath);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error while reading file: " + e.getMessage());
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Error: SHA-256 algorithm not available.");
+            e.printStackTrace();
+        }
+        return null;
     }
     public static void main(String[] args) throws IOException {
         initRepo();
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # git-project-Angela
 # Call the initRepo() method in the main to initialize a git directory and an objects directory, HEAD file, and index file inside it.
 # We initialize, verify, and cleanup the repository through calling methods to test if it can be created and deleted. 
+# A SHA-1 hash function was created to create a hexadecimal string representing the filpath
